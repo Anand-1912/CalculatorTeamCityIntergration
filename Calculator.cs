6836fe222dbbbfdd96514a1ef612e45f9a3abaa2
Anand-1912 <anandchandrasekaran1912@gmail.com>
2022-12-21 12:34:27 +0530

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorTeamCityIntergration1
+{
+    public class Calculator
+    {
+        public int operand1 { get; set; }
+        public int operand2 { get; set; }
+
+        public int Add(int op1, int op2)
+        {
+            return op1 + op2;
+        }
+        public int Sub(int op1, int op2)
+        {
+            return op1 - op2;
+        }
+        public int Multiply(int op1, int op2)
+        {
+            return op1 * op2;
+           
+        }
+        public int Divide(int op1, int op2)
+        {
+            return op1 / op2;
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorTeamCityIntergration1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            Console.WriteLine($"The Sum of 4 and 5 is {calculator.Add(4, 5)}");
+            Console.ReadLine();
+        }
+    }
+}
